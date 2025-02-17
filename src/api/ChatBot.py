e031cabd9415b86efde01cfb3d3d5eff0eea771d
surendra2chandu <surendra2chandu@gmail.com>
2025-02-17 10:43:33 +0530

    update

--- src/api/ChatBot.py
+++ src/api/ChatBot.py
@@ -1,10 +1,9 @@
 # import necessary libraries
-import requests
 from src import logger
-from src.conf.Configurations import RETRIEVAL_URL, SEMANTIC_CONFIGURATION
+from src.conf.Configurations import SEMANTIC_CONFIGURATION
 from src.utilities.OllamaServiceManager import process_ollama_request
 from src.utilities.ChatBotUtilities import ChatBotUtilities
-from fastapi import HTTPException
+from src.retrival.Retrival import Retrival
 
 class ChatBot:
 
@@ -17,46 +16,41 @@ class ChatBot:
         :return: The response from the service
         """
 
-        try:
-            response  = requests.post(RETRIEVAL_URL, params={"query": query, "doc_id": doc_id})
-            if response.status_code == 200:
+        # Get the similer documents
+        logger.info("Getting the similer documents...")
+        similer_documents  = Retrival().get_similer_documents(query, doc_id)
 
-                # Get the similer documents
-                logger.info("Getting the similer documents...")
-                similer_documents = response.json()
+        if similer_documents:
 
-                if SEMANTIC_CONFIGURATION == "BOTH":
+            if SEMANTIC_CONFIGURATION == "BOTH":
 
-                    # Get the text from the semantically similer documents and the Tf-Idf similer documents
-                    logger.info("Getting the text from the semantically similer documents and the Tf-Idf similer documents...")
-                    context = ChatBotUtilities().get_sematic_similer_documents_text(similer_documents) + ChatBotUtilities().get_tf_idf_similer_documents_text(similer_documents)
+                # Get the text from the semantically similer documents and the Tf-Idf similer documents
+                logger.info("Getting the text from the semantically similer documents and the Tf-Idf similer documents...")
+                context = ChatBotUtilities().get_sematic_similer_documents_text(similer_documents) + ChatBotUtilities().get_tf_idf_similer_documents_text(similer_documents)
 
-                elif SEMANTIC_CONFIGURATION == "Tf_Idf":
+            elif SEMANTIC_CONFIGURATION == "Tf_Idf":
 
-                    # Get the text from the Tf-Idf similer documents
-                    logger.info("Getting the text from the Tf-Idf similer documents...")
-                    context = ChatBotUtilities().get_tf_idf_similer_documents_text(similer_documents)
-
-                else:
-
-                    # Get the text from the semantically similer documents
-                    logger.info("Getting the text from the semantically similer documents...")
-                    context = ChatBotUtilities().get_sematic_similer_documents_text(similer_documents)
-
-                if context:
-                    # Process the response with the Ollama model
-                    logger.info("Processing the response with the Ollama model")
-                    response = process_ollama_request(context, query)
-                else:
-                    response = "No relevant information found in the database."
-
-                return response
+                # Get the text from the Tf-Idf similer documents
+                logger.info("Getting the text from the Tf-Idf similer documents...")
+                context = ChatBotUtilities().get_tf_idf_similer_documents_text(similer_documents)
 
             else:
-                return "Error occurred when processing the request to url " + RETRIEVAL_URL + " Error: " + str(response.status_code)
 
-        except Exception as e:
-            return "Error occurred when processing the request to url " + RETRIEVAL_URL + " Error: " + str(e)
+                # Get the text from the semantically similer documents
+                logger.info("Getting the text from the semantically similer documents...")
+                context = ChatBotUtilities().get_sematic_similer_documents_text(similer_documents)
+
+            if context:
+                # Process the response with the Ollama model
+                logger.info("Processing the response with the Ollama model")
+                response = process_ollama_request(context, query)
+            else:
+                response = "No relevant information found in the database."
+
+            return response
+
+        else:
+            return "Error occurred while processing the request "
 
 
 if __name__ == "__main__":
